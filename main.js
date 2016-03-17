f369d11c66a0dd64bdf31bac3324d39c82fa9895
aridoza <aridoza85@gmail.com>
2016-03-17 17:06:13 -0400

    fixed clone files

--- main.js
+++ main.js
@@ -8,18 +8,36 @@ submitButton.addEventListener('click', function(evt){
 
   // var query;
 
+  //avg per household was 911kwH per month; (divide monthly by this to get per household)
+
   $.ajax({
     url: 'http://api.eia.gov/series/?api_key=' + EIA_KEY  + '&series_id=ELEC.CONS_TOT.PEL-' + inputBox.value + '-96.A'
   }).done(function(reply){
       console.log(reply);
+
+      var data = {desc: '', units: '', year1: '', amt1: '', year2: '', amt2: '', year3: '', amt3: ''};
+
+      data.desc = reply.series[0].description;
+      data.units = reply.series[0].units;
+      data.year1 = reply.series[0].data[0][0];
+      data.amt1 =
+      data.year2 =
+      data.amt2 =
+      data.year3 =
+      data.amt3 =
+
+
+
+      var energyData = document.querySelector('.energy-data').innerHTML;
+      var template = Handlebars.compile(energyData);
+      var newHTML = template(data);
+
+      var energyDiv = document.querySelector('.energy-div');
+      energyDiv.innerHTML = newHTML;
+
   })// end .done submit button
 
-// call to get population data for state
-  $.ajax({
-    url: 'http://api.census.gov/data/2015/pep/population?get=POP,GEONAME&for=state:' + inputBox.value + '&key=' + CENSUS_KEY,
-  }).done(function(reply){
-    console.log(reply);
-  })// end pop data .done
+
 
 })// end submit button event listener
 
